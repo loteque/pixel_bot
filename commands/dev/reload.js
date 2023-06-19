db7ecfbc987f0b12fc9e31299fe2d23d9a71a087
loteque <andrewandroid@gmail.com>
2023-06-19 07:24:24 +0200

    update reload make ephemeral

--- commands/dev/reload.js
+++ commands/dev/reload.js
@@ -14,7 +14,7 @@ module.exports = {
         const command = interaction.client.commands.get(CommandName);
 
         if (!command) {
-            return interaction.reply(`There is no command with name \`${CommandName}\`!`);
+            return interaction.reply({ content: `There is no command with name \`${CommandName}\`!`, ephemeral: true });
         }
 
         delete require.cache[require.resolve(`../${command.category}/${command.data.name}.js`)]
@@ -23,10 +23,10 @@ module.exports = {
             interaction.client.commands.delete(command.data.name);
             const newCommand = require(`../${command.category}/${command.data.name}.js`);
             interaction.client.commands.set(newCommand.data.name, newCommand);
-            await interaction.reply(`Command \`${newCommand.data.name}\` was reloaded!`);
+            await interaction.reply({ content: `Command \`${newCommand.data.name}\` was reloaded!`, ephemeral: true });
         } catch (error) {
             console.error(error);
-            await interaction.reply(`There was an error while reloading a command\`${command.data.name}\`:\n\`${error.message}\``);
+            await interaction.reply({ content: `There was an error while reloading a command\`${command.data.name}\`:\n\`${error.message}\``, ephemeral: true });
         }
     },
 };
