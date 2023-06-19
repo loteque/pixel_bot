645d7e744c5ca48b4fe98b32df69f1fa283a3a1f
loteque <andrewandroid@gmail.com>
2023-06-19 14:10:11 +0200

    add echo command

---
+++ commands/utility/echo.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('discord.js');
+const path = require('node:path')
+
+module.exports = {
+    category: path.basename(path.resolve(__dirname)),
+		data: new SlashCommandBuilder()
+        .setName(path.parse(path.basename(path.resolve(__filename))).name)
+		.setDescription('Replies with your input!')
+        .addStringOption(option =>
+            option.setName('input')
+                .setDescription('the input to echo back'))
+        .addChannelOption(option => 
+            option.setName('channel')
+                .setDescription('The channel to echo into')),
+	async execute(interaction) {
+        const input = interaction.options.getString('input')
+        const channel = interaction.options.getChannel('channel')
+		await channel.send({ content: `${input}`, ephemeral: true})
+	},
+};
