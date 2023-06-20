88f760514bed2b3d7802b3005977386afe362a02
loteque <andrewandroid@gmail.com>
2023-06-20 12:24:52 +0200

    Add handler for case where channel already exists

--- commands/dev/project.js
+++ commands/dev/project.js
@@ -9,9 +9,9 @@ module.exports = {
         .addSubcommand(subcommand =>
             subcommand
                 .setName('create')
-                .setDescription('Create a project channel!')
+                .setDescription('Create a project forum!')
                 .addStringOption(option => 
-                    option.setName('project')
+                    option.setName('name')
                         .setDescription('Project Name')
                         .setRequired(true))
                 .addChannelOption(option => 
@@ -25,41 +25,47 @@ module.exports = {
                 .setDescription('Info about the channel')
                 .addChannelOption(option => 
                     option.setName('channel')
+                        .addChannelTypes(15)
                         .setDescription('The channel to get info about')
                         .setRequired(true))),
 	async execute(interaction) {
-        const channelName = interaction.options.getString('project');
+        const channelName = interaction.options.getString('name');
         const channel = interaction.options.getChannel('channel')
         const categoryName = interaction.options.getChannel('category')
+        const existingChannel = interaction.guild.channels.cache.find(ch => ch.name === channelName)
 
-		if (interaction.options.getSubcommand() === 'create'){
-            const category = interaction.guild.channels.cache.find(category => category.name == categoryName.name)
+        if (interaction.options.getSubcommand() === 'create'){
+		    if (existingChannel){
+                await interaction.reply({ content: `Project forum exists, please choose another project name\nChannel Name: ${existingChannel.name}\nChannel Category: ${existingChannel.parent.name}`, ephemeral: true });
+            } else {
+                const category = interaction.guild.channels.cache.find(category => category.name == categoryName.name)
 
-            await interaction.guild.channels.create({
-                name: channelName,
-                type: ChannelType.GuildForum,
-                parent: category.id
-            });
-            const projectChannel = interaction.guild.channels.cache.find(ch => ch.name == channelName)
-            await projectChannel.threads.create({
-                name: 'github-notify',
-                autoArchiveDuration: 10080,
-                message: {
-                    content: 'Github Repository Updates'
-                }
-            });
-            const thread = projectChannel.threads.cache.find(t => t.name === 'github-notify')
-            await projectChannel.createWebhook({
-                name: 'github-notify'
-            });
-            const webhooks = await projectChannel.fetchWebhooks();
-            const webhook = webhooks.find(wh => wh.name === 'github-notify')
+                await interaction.guild.channels.create({
+                    name: channelName,
+                    type: ChannelType.GuildForum,
+                    parent: category.id
+                });
+                const projectChannel = interaction.guild.channels.cache.find(ch => ch.name == channelName)
+                await projectChannel.threads.create({
+                    name: 'github-notify',
+                    autoArchiveDuration: 10080,
+                    message: {
+                        content: 'Github Repository Updates'
+                    }
+                });
+                const thread = projectChannel.threads.cache.find(t => t.name === 'github-notify')
+                await projectChannel.createWebhook({
+                    name: 'github-notify'
+                });
+
+                const webhooks = await projectChannel.fetchWebhooks();
+                const webhook = webhooks.find(wh => wh.name === 'github-notify')
     
-            await interaction.reply({ 
-                content: `project ${channelName} created at ${category.name}\nWebhook URL: https://discord.com/api/webhooks/${webhook.id}/${webhook.token}/github?thread_id=${thread.id}`,
-                ephemeral: true 
-            });
-
+                await interaction.reply({ 
+                    content: `project ${channelName} created at ${category.name}\nWebhook URL: https://discord.com/api/webhooks/${webhook.id}/${webhook.token}/github?thread_id=${thread.id}`,
+                    ephemeral: true 
+                });
+            }
         } else if (interaction.options.getSubcommand() === 'info') {
 			await interaction.reply({ content: `Channel Name: ${channel.name}\nChannel Category: ${channel.parent.name}`, ephemeral: true });
 		}
